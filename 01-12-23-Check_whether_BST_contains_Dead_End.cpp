c7d913b6ad27a0c346f7123190a439658f2c2452
Yashwant Choudhary <84570757+Yashwant077@users.noreply.github.com>
2023-12-04 10:12:37 +0530

    Create 01-12-23-Check_whether_BST_contains_Dead_End.cpp

---
+++ 01-12-23-Check_whether_BST_contains_Dead_End.cpp
@@ -0,0 +1,171 @@
+/*
+Problem Description: Check whether BST contains Dead End
+
+Given a Binary Search Tree that contains unique positive integer values greater than 0. The task is to complete the function isDeadEnd which returns true if the BST contains a dead end else returns false. Here Dead End means a leaf node, at which no other node can be inserted.
+
+Example 1:
+
+Input :   
+               8
+             /   \ 
+           5      9
+         /  \     
+        2    7 
+       /
+      1     
+          
+Output : 
+Yes
+Explanation : 
+Node 1 is a Dead End in the given BST.
+Example 2:
+
+Input :     
+              8
+            /   \ 
+           7     10
+         /      /   \
+        2      9     13
+
+Output : 
+Yes
+Explanation : 
+Node 9 is a Dead End in the given BST.
+Your Task: You don't have to input or print anything. Complete the function isDeadEnd() which takes BST as input and returns a boolean value.
+
+Expected Time Complexity: O(N), where N is the number of nodes in the BST.
+Expected Space Complexity: O(N)
+
+Constraints:
+1 <= N <= 1001
+1 <= Value of Nodes <= 10001
+
+*/
+
+
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+void insert(Node ** tree, int val)
+{
+    Node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = new Node(val);
+        *tree = temp;
+        return;
+    }
+
+    if(val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if(val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+
+}
+
+
+
+
+int getCountOfNode(Node *root, int l, int h)
+{
+
+    if (!root) return 0;
+
+
+    if (root->data == h && root->data == l)
+        return 1;
+
+    if (root->data <= h && root->data >= l)
+         return 1 + getCountOfNode(root->left, l, h) +
+                    getCountOfNode(root->right, l, h);
+
+    else if (root->data < l)
+         return getCountOfNode(root->right, l, h);
+
+
+    else return getCountOfNode(root->left, l, h);
+}
+
+
+// } Driver Code Ends
+/*The Node structure is
+struct Node {
+int data;
+Node * right, * left;
+};*/
+
+/*You are required to complete below method */
+class Solution{
+  public:
+  
+    bool helper(Node* node, int MIN, int MAX){
+        if(!node)
+            return 0;
+            
+        if(node->data==MIN and node->data==MAX)
+            return 1;
+            
+        return helper(node->left, MIN, node->data-1) or helper(node->right, node->data+1,MAX);
+    }
+    
+    bool isDeadEnd(Node *root)
+    {
+        //Your code here
+        return helper(root,1,1e8);
+    }
+};
+
+//{ Driver Code Starts.
+// bool isDeadEnd(Node *root);
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Node *root;
+        Node *tmp;
+    //int i;
+
+        root = NULL;
+
+        int N;
+        cin>>N;
+        for(int i=0;i<N;i++)
+        {
+            int k;
+            cin>>k;
+            insert(&root, k);
+
+        }
+
+     Solution ob;
+     cout<<ob.isDeadEnd(root);
+     cout<<endl;
+    }
+}
+
+
+// } Driver Code Ends
